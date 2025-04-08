14a222ea3752924d7cc7f6ee34451cd1a873c53c
Alfar1to <andreialfaro07@gmail.com>
2025-04-08 13:25:39 -0700

    hola

--- index.js
+++ index.js
@@ -16,11 +16,11 @@
 
 //fetchData()
 
-autocompleteConfig = {
+autoCompleteConfig = {
     renderOption(movie){
         const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
-        return `
-            <img src = "${imgSrc}"/>
+        return`
+            <img src="${imgSrc}" />
             ${movie.Title} (${movie.Year})
         `
     },
@@ -28,10 +28,13 @@ autocompleteConfig = {
         return movie.Title
     },
     async fetchData(searchTerm) {
-        apiMovieURL = 'https://omdbapi.com/'
+        console.log("Hello!")
+        apiMovieURL = 'http://www.omdbapi.com/'
         const response = await axios.get(apiMovieURL, {
-            params: '65904a8a',
-            s: searchTerm
+            params: {
+                apikey: '5db51dbd',
+                s: searchTerm
+            }
         })
         if(response.data.Error){
             return []
@@ -43,58 +46,58 @@ autocompleteConfig = {
 }
 
 createAutoComplete({
-    ...autocompleteConfig,
+    ...autoCompleteConfig,
     root: document.querySelector('#left-autocomplete'),
-    onOptionSelect(Movie){
+    onOptionSelect(movie){
         document.querySelector('.tutorial').classList.add('is-hidden')
-        onMovieSelect(movie, document.querySelector('#left-summery'), 'left')
+        onMovieSelect(movie, document.querySelector('#left-summary'), 'left')
     }
 })
 
 createAutoComplete({
-    ...autocompleteConfig,
+    ...autoCompleteConfig,
     root: document.querySelector('#right-autocomplete'),
-    onOptionSelect(Movie){
+    onOptionSelect(movie){
         document.querySelector('.tutorial').classList.add('is-hidden')
-        onMovieSelect(movie, document.querySelector('#right-summery'), 'right')
+        onMovieSelect(movie, document.querySelector('#right-summary'), 'right')
     }
 })
 
-//Crear dos variables para left Movie y right Movie
-
+//Crear dos variables para leftMovie y rightMovie
 let leftMovie
 let rightMovie
 
-const onMovieSelect = async (movie,summaryElement, side) => {
-    const response = await axios.get("https://omdbapi.com/",{
+const onMovieSelect = async (movie, summaryElement, side) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
         params: {
-            apikey: '65904a8a',
-            i: movie.imbID
+            apikey: '5db51dbd',
+            i: movie.imdbID
         }
     })
     console.log(response.data)
     summaryElement.innerHTML = movieTemplate(response.data)
 
-    //Preguntame cual lado es
+    // Preguntamos cual lado es?
     if(side === 'left'){
-        leftMovie = response.data
+        leftMovie = response.data   
     }else{
         rightMovie = response.data
     }
-    //Preguntamos si tenemos ambos lados
+
+    // Preguntamos si temos ambos lados
     if(leftMovie && rightMovie){
-        // Entonces ejecutamos la funcion comparacion
-        runComparasion()
+        // Entonces ejecutamos la funcion de comparacion
+        runComparison()
     }
 }
 
-const runComparasion = () => {
-    console.log('Comparasion de peliculas')
+const runComparison = () => {
+    console.log('Comparacion de peliculas')
     const leftSideStats = document.querySelectorAll('#left-summary .notification')
     const rightSideStats = document.querySelectorAll('#right-summary .notification')
 
     leftSideStats.forEach((leftStat, index) => {
-        const rightStat = rightSideStats(index)
+        const rightStat = rightSideStats[index]
         const leftSideValue = parseInt(leftStat.dataset.value)
         const rightSideValue = parseInt(rightStat.dataset.value)
 
@@ -105,17 +108,20 @@ const runComparasion = () => {
             rightStat.classList.remove('is-primary')
             rightStat.classList.add('is-danger')
         }
+
     })
+
 }
-const movieTemplates = (moviedetails) => {
-    //transformar a numeros los string que llegan de los datos
-    const dollars = parseInt(moviedetails.BoxOffice.replace(/\$/g,'').replace(/,/g,''))
+
+const movieTemplate = (movieDetails) => {
+    // Transformar a numeros los strings que llegan de los datos
+    const dollars = parseInt(movieDetails.BoxOffice.replace(/\$/g, '').replace(/,/g, ''))
     console.log(dollars)
-    const metascore = parseInt(moviedetails.metascore)
-    const imdbRating = parseFloat(moviedetails.imdbRating)
-    const imdbVotes =  parseInt(moviedetails.imdbVotes.replace(/,/g,''))
+    const metascore = parseInt(movieDetails.Metascore)
+    const imdbRating = parseFloat(movieDetails.imdbRating)
+    const imdbVotes = parseInt(movieDetails.imdbVotes.replace(/,/g, ''))
     console.log(metascore, imdbRating, imdbVotes)
-    const awards = moviedetails.awards.split('').reduce((prev,word)=> {
+    const awards = movieDetails.Awards.split('').reduce((prev, word) => {
         const value = parseInt(word)
 
         if(isNaN(value)){
@@ -124,140 +130,45 @@ const movieTemplates = (moviedetails) => {
             return prev + value
         }
     }, 0)
-    console.log('Awards', awards)  
-    //agregar la propiedad data-value a cada elemento del template
+    console.log('Awards', awards)
+    
+    // Agregar la propiedad data-value a cada elemento del template
 
     return `
         <article class="media">
             <figure class="media-left">
                 <p class="image">
-                    <img src="${moviedetails.Poster}"/>
+                    <img src="${movieDetails.Poster}"/>
                 </p>
             </figure>
-        
-        <div class="media-content">
-            <div class="content">
-                <h1>${moviedetails.Title}</h1>
-                <h4>${moviedetails.Genre}</h4>
-                <p>${moviedetails.Plot}</p>
-            <div>
-        </div>
+            <div class="media-content">
+                <div class="content">
+                    <h1>${movieDetails.Title}</h1>
+                    <h4>${movieDetails.Genre}</h4>
+                    <p>${movieDetails.Plot}</p>
+                </div>
+            </div>
         </article>
-        <article data-values=${awards} clas="notification is-primary">
-            <p class="title">${moviedetails.awards}</p>
+        <article data-values=${awards} class="notification is-primary">
+            <p class="title">${movieDetails.Awards}</p>
             <p class="subtitle">Awards</p>
         </article>
-        <article data-values=${dollars} clas="notification is-primary">
-            <p class="title">${moviedetails.BoxOffice}</p>
+        <article data-values=${dollars} class="notification is-primary">
+            <p class="title">${movieDetails.BoxOffice}</p>
             <p class="subtitle">Box Office</p>
         </article>
-        <article data-values=${metascore} clas="notification is-primary">
-            <p class="title">${moviedetails.metascore}</p>
+        <article data-values=${metascore} class="notification is-primary">
+            <p class="title">${movieDetails.Metascore}</p>
             <p class="subtitle">Metascore</p>
         </article>
-        <article data-values=${imdbRating} clas="notification is-primary">
-            <p class="title">${moviedetails.imdbRating}</p>
+        <article data-values=${imdbRating} class="notification is-primary">
+            <p class="title">${movieDetails.imdbRating}</p>
             <p class="subtitle">IMDB Rating</p>
         </article>
-        <article data-values=${imdbVotes} clas="notification is-primary">
-            <p class="title">${moviedetails.imdbVotes}</p>
+        <article data-values=${imdbVotes} class="notification is-primary">
+            <p class="title">${movieDetails.imdbVotes}</p>
             <p class="subtitle">IMDB Votes</p>
-        </article>
-    `
-
-}
-
-
-
-const root = document.querySelector('.autocomplete')
-root.innerHTML = `
-    <label><b> Busqueda de Peliculas </b></label>
-    <input class = "input" />
-    <div class = "dropdown">
-        <div class = "dropdown-menu">
-            <div class = "dropdown-content results"></div>
-        </div>
-    </div>
-`
-const input = document.querySelector("inputs")
-const dropdown = document.querySelector(".dropdown")
-const resultsWeapper = document.querySelector(".results")
-
-const debonce = (func, delay = 1000) => {
-    let timeoutId
-    return(...arg) => {
-        clearTimeout(timeoutId)
-        timeoutId = setTimeout(() => {
-            func.apply(null, arg)
-        }, delay)
-    }
-}
-
-const onInput = async(event) => {
-    const movies = await fetchData(event.target.values)
-    console.log("MOVIES: ",movies)
-    
-    if(!movies.length){
-        dropdown.classList.remove('is-activate')
-        return
-    }
-    resultsWeapper.innerHTML = ''
-    dropdown.classList.add('is-activate')
-
-    for (let movie of movies){
-        const option = document.createElement('a')
-        const imgSrc = movie.Poster === "N/A" ? '': movie.Poster
-
-        option.classList.add('dropdown-item')
-        option.innerHTML = `
-            <img src="${imgSrc}" />
-            ${movie.Title}
-        `
-        option.addEventListener('click', () => {
-            dropdown.classList.remove('is-activate')
-            input.value = movie.Title
-            onMoviesSelect(movie)
-        })
-        resultsWeapper.appendChild(option)
-    }
-}
-
-input.addEventListener('input', debonce(onInput, 1000))
-
-document.addEventListener('click', event => {
-    if(!root.contains(event.target)){
-        dropdown.classList.remove('is-activate')
-    }
-})
-
-const onMoviesSelect = async(movie) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: '65904a8a',
-            i: movie.imbID
-        }
-    })
-
-    console.log(response.data)
-    document.querySelector('summary').innerHTML = movieTemplate(response.data)
-}
-
-const movieTemplate = (movieData1) => {
-    return `
-        <articule class="media">
-            <figure class="media-left">
-                <p class="image">
-                    <img src="${movieData1.Poster}" />
-                </p>
-            </figure>
-            <div class = ""media-content">
-                <div class="content">
-                    <h1>${movieData1.Title}</h1>
-                    <h4>${movieData1.Genre}</h4>
-                    <p>${movieData1.Plot}</p> 
-                </div>
-            </div>    
-        </articule>
-    
+        </article>   
     `
 }
+
